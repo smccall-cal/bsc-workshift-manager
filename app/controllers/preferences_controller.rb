8239e4749041aced139228cf1ee099cae8502e2a
DanTNC <fad11204@yahoo.com.tw>
2017-10-21 07:59:33 +0000

    implement needing login-check in any steps of preferences

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,6 +1,6 @@
 class PreferencesController < ApplicationController
-  before_action :set_user
-  before_action :set_preference, only: [:show, :edit, :update]
+  before_action :set_user, :except => :notlogged
+  before_action :set_preference, :only => [:show, :edit, :update]
 
   def index
     if @user.preferences.length == 0
@@ -11,6 +11,9 @@ class PreferencesController < ApplicationController
   end
 
   def no
+  end
+  
+  def notlogged
   end
 
   # GET /preferences/1
@@ -56,6 +59,15 @@ class PreferencesController < ApplicationController
       end
     end
   end
+  
+  def logged_in
+      session_id = params[:session_id] || session[:session_id]
+      @user = User.find_by(session_id: session_id)
+      session_id != nil && @user != nil
+      # the following stubs for human test
+      @user = User.find(params[:user_id])
+      true
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -64,11 +76,12 @@ class PreferencesController < ApplicationController
     end
     def set_user
       @user = User.find(params[:user_id])
+      if not logged_in then redirect_to notlogged_user_preferences_path(params[:user_id]) end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def preference_params
       {:shift=>params[:preference][:shift], :schedule=>params[:preference][:schedule]}
-      # params.require(:preference).permit(:shift, :schedule)
+      # params.require(:preference).permit(:shift, :schedule) to be implemented
     end
 end
