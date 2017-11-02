28d4e0802896120ae8c55dc3755789ce00affc45
DanTNC <fad11204@yahoo.com.tw>
2017-11-02 08:43:37 +0000

    [Feature] rspec test for ApplicationController (first-time-user)

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+    
+    controller do
+        def index
+            render :plain => "Hello world"
+        end
+    end
+    
+    before(:each) do
+        @user = User.new
+    end
+    
+    describe "before_action :new_password" do
+        it "doesn't check first-time-user if no user's logged in" do
+            allow(controller).to receive(:current_user).and_return(nil)
+            get :index
+            expect(response.body).to eq "Hello world"
+        end
+        
+        it "redirect user to set new password if it's first time loggin" do
+            allow(controller).to receive(:current_user).and_return(@user)
+            allow(@user).to receive(:init).and_return(true)
+            get :index
+            expect(response).to redirect_to edit_user_registration_path
+        end
+        
+        it "doesn't redirect user to set new password if it's not first time loggin" do
+            allow(controller).to receive(:current_user).and_return(@user)
+            allow(@user).to receive(:init).and_return(false)
+            get :index
+            expect(response.body).to eq "Hello world"
+        end
+    end
+end
