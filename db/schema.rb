db4a83846f19dfb99ce89a2d4c96233856b9c3b6
DanTNC <fad11204@yahoo.com.tw>
2017-11-02 07:41:22 +0000

    [Fix] correct default value of init col of users (to true)

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20171102054933) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.boolean "init", default: false
+    t.boolean "init", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
