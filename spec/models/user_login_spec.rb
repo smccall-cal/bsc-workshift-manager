42c31cef9e439d5bb48683f8c7abf84cf75b868f
Spencer McCall <smccall@Spencers-MacBook-Air.local>
2017-11-11 17:17:32 -0800

    [Chore] Increased number of Rspec tests for User model

--- spec/models/user_login_spec.rb
+++ spec/models/user_login_spec.rb
@@ -19,7 +19,7 @@ describe User do
         User.init(@user, @email, @pass, @building)
     end
 
-    it "correctly hides the password" do
+    it "hides the password" do
         User.init(@user, @email, @pass, @building)
         expect(User.find_by(username: @user).encrypted_password).not_to eq(@pass)
     end
@@ -27,6 +27,39 @@ describe User do
     it "properly saves" do
         v = User.init(@user, @email, @pass, @building)
         expect(v).to eq(true)
+    end
+
+    it "defaults to User privileges" do
+        User.init(@user, @email, @pass, @building)
+        expect(User.find_by(username: @user).manage?).to eq(false)
+    end
+
+    it "promotes to Manager privileges" do
+        User.init(@user, @email, @pass, @building)
+        u = User.find_by(username: @user)
+        u.promote("Manager")
+        expect(u.manage?).to eq(true)
+    end
+
+    it "promotes to Admin privileges" do
+        User.init(@user, @email, @pass, @building)
+        u = User.find_by(username: @user)
+        u.promote("Admin")
+        expect(u.admin?).to eq(true)
+    end
+
+    it "gives Admins the Manager privileges" do
+        User.init(@user, @email, @pass, @building)
+        u = User.find_by(username: @user)
+        u.promote("Admin")
+        expect(u.manage?).to eq(true)
+    end
+
+    it "does not give Managers the Admin privileges" do
+        User.init(@user, @email, @pass, @building)
+        u = User.find_by(username: @user)
+        u.promote("Manager")
+        expect(u.admin?).to eq(false)
     end
 
 end
