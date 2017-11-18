cf5b0fd1a218d9df1a0808e3eb548cbb42ac1aa6
Spencer McCall <smccall@Spencers-MacBook-Air.local>
2017-11-17 20:29:01 -0800

    [Feature] Skeleton for Mass Add completed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,6 +11,17 @@ class RegistrationsController < Devise::RegistrationsController
         super
     end
 
+    def mass_new
+    end
+
+    def mass_create
+        list = params[:names]
+        emails = params[:emails]
+        building = params[:building]
+        #Split by regex
+        User.init(username: name, email: email, password: "5108481936", building: building)
+    end
+
     protected
         def after_update_path_for(resource)
             new_user_preference_path(current_user.id)
