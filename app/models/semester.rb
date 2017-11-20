8673912794487fab18a74b951853494cf353daf5
Ng Tze Yang <ngtzeyang94@gmail.com>
2017-11-20 03:23:40 +0000

    [fix] relationship between semester and shift_template

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,5 +1,5 @@
 class Semester < ApplicationRecord
-    has_and_belongs_to_many :shifts, dependent: :destroy
+    has_and_belongs_to_many :shift_templates, dependent: :destroy
     
     def self.init(semester_name, start_date, end_date)
         new_semester = Semester.new(semester_name: semester_name, start_date: start_date, end_date: end_date)
