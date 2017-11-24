dc200f8ab655de5527b30208686f1f8b04fc3378
DanTNC <fad11204@yahoo.com.tw>
2017-11-24 01:45:54 +0000

    [Fix] modify rspec tests to fit the change

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe RegistrationsController, type: :controller do
         end
 
         it "adds all the users" do
-            post :mass_create, :params => {user: {usernames: @names, emails: @emails, building: "CZ"}}
+            post :mass_create, :params => {user: {username: @names, email: @emails, building: "CZ"}}
             expect(User.find_by(username: "A")).to be_truthy
             expect(User.find_by(username: "B")).to be_truthy
             expect(User.find_by(username: "C")).to be_truthy
