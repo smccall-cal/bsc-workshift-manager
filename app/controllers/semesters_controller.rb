c05400fb5f13fa8c9503cfd95edd6efa90a8d663
Ng Tze Yang <ngtzeyang94@gmail.com>
2017-11-29 07:57:18 +0000

    [feature] added deleting semesters path

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,6 +1,6 @@
 class SemestersController < ApplicationController
   before_action :manager?
-  #before_action :set_semester, only: [:show, :edit, :update, :destroy]
+  before_action :set_semester, only: [:show, :edit, :update, :destroy]
 
   # GET /semesters
   # GET /semesters.json
@@ -11,7 +11,6 @@ class SemestersController < ApplicationController
   # GET /semesters/1
   # GET /semesters/1.json
   def show
-      @semester = Semester.find(params[:id])
       session[:semester] = @semester
       @shift_templates = @semester.shift_templates.all
   end
@@ -23,7 +22,6 @@ class SemestersController < ApplicationController
 
   # GET /semesters/1/edit
   def edit
-    @semester = Semester.find(params[:id])
   end
 
   # POST /semesters
@@ -51,29 +49,25 @@ class SemestersController < ApplicationController
   # PATCH/PUT /semesters/1
   # PATCH/PUT /semesters/1.json
   def update
-    @semester = Semester.find(params[:id])
-      if @semester.update(semester_params)
-            redirect_to semester_path(@semester), notice: 'User was successfully updated.'
-        else
-            redirect_to semesters_path, alert: @semester.errors.full_messages[0]
-        end
+    if @semester.update(semester_params)
+        redirect_to semester_path(@semester), notice: 'User was successfully updated.'
+    else
+        redirect_to semesters_path, alert: @semester.errors.full_messages[0]
+    end
   end
 
   # DELETE /semesters/1
   # DELETE /semesters/1.json
-  # def destroy
-  #   @semester.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to semesters_url, notice: 'Semester was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
+  def destroy
+    @semester.destroy
+    redirect_to semesters_path
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_semester
-    #   @semester = Semester.find(params[:id])
-    # end
+    def set_semester
+      @semester = Semester.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     # def semester_params
