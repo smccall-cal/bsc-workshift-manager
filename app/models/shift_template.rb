bb59264ea7684588c172778d6d809cc9e3f43be0
Spencer McCall <smccall.cal@gmail.com>
2017-12-02 15:56:47 -0800

    [Refactor] Cleaning up database

--- app/models/shift_template.rb
+++ app/models/shift_template.rb
@@ -3,6 +3,7 @@ class ShiftTemplate < ApplicationRecord
     has_many :shifts
     belongs_to :shift_detail
     belongs_to :user, optional: true
+
     def self.init(template)
         shift_detail = ShiftDetail.where(location: template[:location], description: template[:description]).take
         @shift_template = shift_detail.shift_templates.create!(hours: template[:hours], day: template[:day], floor: template[:floor], details: template[:details], user_id: template[:user_id])
@@ -26,11 +27,25 @@ class ShiftTemplate < ApplicationRecord
     end
 
     def name
-        return ShiftDetail.find(self.shift_detail_id).to_sym
+        @details ||= ShiftDetail.find(self.shift_detail_id)
+        return @details.to_sym
+    end
+
+    def unique
+        @details ||= ShiftDetail.find(self.shift_detail_id)
+        return (@details.to_s + " " + self.specifics " : "  + self.date_time).to_sym
+    end
+
+    def specifics
+        return "(" + self.details + " " + self.floor + ")"
+    end
+
+    def date_time
+        return self.day + " " + self.time
     end
 
     def time
-        return "1:00"
+        return "1pm"
     end
 
 end
